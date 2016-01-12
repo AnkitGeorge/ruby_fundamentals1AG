51c62c66b80675a46153343e02294885b35ee53d
AnkitGeorge <ankit.george@gmail.com>
2016-01-12 14:13:46 -0500

    Add exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,23 @@
+# #part 1
+# pre_tip_total = 55
+#
+# tip = pre_tip_total * 0.2
+# puts "A good tip for a $55 meal would be $#{tip}."
+#
+# total = pre_tip_total * 1.2
+# puts "The total for the meal would be $#{total}."
+
+
+# # part 2
+# "5" + 2
+#
+# #this attempt yields-->
+# #exercise2.rb:11:in `+': no implicit conversion of Fixnum into String (TypeError)
+# #from exercise2.rb:11:in `<main>'
+
+
+# #part 3 -- string interpolation
+# puts "45628 multiplied by 7839 is #{45628*7839}."
+
+# part 4 -- boolean fun
+puts ( 10 < 20 && 30 < 20 ) || !( 10 == 11 )
