c995a1da7543f74a42c0cd811b72e28e62331758
AnkitGeorge <ankit.george@gmail.com>
2016-01-12 14:49:38 -0500

    Ask user for age, outputs year born.

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+puts "\n\nWhat is your age?"
+age = Integer(gets.chomp)
+puts "You were born in the year #{2016 - age}"
+puts "Be nice, I have not accounted for the month and day :)"
